b49c48fa9730ec8af688f9ace86540b3707f799d
actionshark <sunhongkao@qq.com>
2018-11-02 14:22:55 +0800

    支持自定义端口号

--- src/com/kk/hitplane/Main.java
+++ src/com/kk/hitplane/Main.java
@@ -5,11 +5,21 @@ import com.kk.hitplane.log.Logger;
 
 public class Main {
 	public static void main(String[] args) {
+		int port = 10001;
+		
+		for (int i = 0; i < args.length; i++) {
+			String arg = args[i];
+			
+			if ("-port".equals(arg)) {
+				port = Integer.parseInt(args[++i]);
+			}
+		}
+		
 		FileLogger logger = new FileLogger();
 		logger.setFiles("log1.txt", "log2.txt");
 		Logger.setInstance(logger);
 
-		Server.init(10001);
+		Server.init(port);
 		Server server = Server.getInstance();
 		server.start();
 	}
